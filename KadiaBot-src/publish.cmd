42fa0ecb1cb1c2be9b564b77e8d2d3e4c10f6963
Karla Alvarez <karlaad59@gmail.com>
2018-02-17 09:49:26 -0500

    first commit

---
+++ KadiaBot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=kadiabot-Web-Deploy.pubxml -p:Password=XgWEGwlpuDoLMjf5zssfzvL3YHBueKwEJc50AtAQxpQ6B5cgJQ59jdRGZH8Z
+
